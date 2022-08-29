460d0bce12c686d541c78a3ff80b43ba3a0cb0d7
MauricioHdz <mauricioisraelhernandez@gmail.com>
2022-08-29 15:36:45 -0500

    v.0.7.2 Se integra relacion sucursal registro_patronal

--- orm/im_registro_patronal.php
+++ orm/im_registro_patronal.php
@@ -6,7 +6,7 @@ use PDO;
 class im_registro_patronal extends modelo{
     public function __construct(PDO $link){
         $tabla = __CLASS__;
-        $columnas = array($tabla=>false, 'fc_cfd' => $tabla,'im_clase_riesgo' => $tabla);
+        $columnas = array($tabla=>false, 'fc_cfd' => $tabla, 'org_sucursal' => 'fc_cfd','im_clase_riesgo' => $tabla);
         $campos_obligatorios = array();
 
         parent::__construct(link: $link,tabla:  $tabla, campos_obligatorios: $campos_obligatorios,
