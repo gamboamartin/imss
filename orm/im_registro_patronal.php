1267e9ad157481ffeb1c0b8dd3bb14d5775f1fff
martingamboaivitec <martin.gamboa@ivitec.com.mx>
2022-08-31 09:43:29 -0500

    v.0.10.2 Se ajusta csd imss

--- orm/im_registro_patronal.php
+++ orm/im_registro_patronal.php
@@ -6,7 +6,7 @@ use PDO;
 class im_registro_patronal extends modelo{
     public function __construct(PDO $link){
         $tabla = __CLASS__;
-        $columnas = array($tabla=>false, 'fc_cfd' => $tabla, 'org_sucursal' => 'fc_cfd',
+        $columnas = array($tabla=>false, 'fc_csd' => $tabla, 'org_sucursal' => 'fc_csd',
             'org_empresa' => 'org_sucursal','im_clase_riesgo' => $tabla);
         $campos_obligatorios = array();
 
