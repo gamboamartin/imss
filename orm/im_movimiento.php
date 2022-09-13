4f98fb8b5c617dec1a474558cdbf465874dec002
martingamboaivitec <martin.gamboa@ivitec.com.mx>
2022-09-13 11:22:22 -0500

    v.0.15.3 Implemenatciuon funcion get ult mov

--- orm/im_movimiento.php
+++ orm/im_movimiento.php
@@ -43,13 +43,30 @@ class im_movimiento extends modelo{
     }
 
 
-    private function filtro_movimiento_fecha(mixed $data,string $fecha): stdClass|array
+    private function filtro_movimiento_fecha(int $em_empleado_id,string $fecha): stdClass|array
     {
-        if ($data['fecha'] === $fecha) {
-            return $data;
+        if ($em_empleado_id <= -1) {
+            return $this->error->error(mensaje: 'Error id del empleado no puede ser menor a uno', data: $em_empleado_id);
         }
 
-        return array();
+
+        $filtro['em_empleado.id'] = $em_empleado_id;
+        $order['im_movimiento.fecha'] = 'DESC';
+        $filtro_extra[0]['im_movimiento.fecha']['valor'] = $fecha;
+        $filtro_extra[0]['im_movimiento.fecha']['operador'] = '>=';
+        $filtro_extra[0]['im_movimiento.fecha']['condicion'] = 'AND';
+        $im_movimiento = $this->obten_datos_ultimo_registro(
+            filtro: $filtro, filtro_extra: $filtro_extra, order: $order);
+
+        if (errores::$error) {
+            return $this->error->error(mensaje: 'Error al obtener el movimiento del empleado', data: $im_movimiento);
+        }
+
+        if (count($im_movimiento) === 0) {
+            return $this->error->error(mensaje: 'Error no hay registros para el empleado', data: $em_empleado_id);
+        }
+
+        return $im_movimiento;
     }
 
 
