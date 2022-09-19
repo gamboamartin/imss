314660a472ed3a29b0ae900f2a314a3b4389f1e2
MauricioHdz <mauricioisraelhernandez@gmail.com>
2022-09-19 16:05:33 -0500

    v0.23.3 Se integra relaciones movimiento

--- orm/im_movimiento.php
+++ orm/im_movimiento.php
@@ -10,7 +10,8 @@ use stdClass;
 class im_movimiento extends modelo{
     public function __construct(PDO $link){
         $tabla = __CLASS__;
-        $columnas = array($tabla=>false,'em_empleado' => $tabla);
+        $columnas = array($tabla=>false,'em_empleado' => $tabla, 'im_registro_patrona'=>$tabla,
+            'im_tipo_movimiento'=>$tabla);
         $campos_obligatorios = array('im_registro_patronal_id','im_tipo_movimiento_id','em_empleado_id','fecha');
 
         parent::__construct(link: $link,tabla:  $tabla, campos_obligatorios: $campos_obligatorios,
