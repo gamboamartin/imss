f22ce9993dd5ee97a7372ce34aef8adb7674e686
mgamboa <gamboa.martin@gmail.com>
2022-09-20 16:12:04 -0500

    v.0.26.4 Se integran llaves de cp a rp

--- orm/im_registro_patronal.php
+++ orm/im_registro_patronal.php
@@ -7,7 +7,8 @@ class im_registro_patronal extends modelo{
     public function __construct(PDO $link){
         $tabla = __CLASS__;
         $columnas = array($tabla=>false, 'fc_csd' => $tabla, 'org_sucursal' => 'fc_csd',
-            'org_empresa' => 'org_sucursal','im_clase_riesgo' => $tabla);
+            'org_empresa' => 'org_sucursal','im_clase_riesgo' => $tabla,'dp_calle_pertenece'=>'org_sucursal',
+            'dp_colonia_postal'=>'dp_calle_pertenece','dp_cp'=>'dp_colonia_postal');
         $campos_obligatorios = array('im_clase_riesgo_id','fc_csd_id','descripcion_select');
 
         parent::__construct(link: $link,tabla:  $tabla, campos_obligatorios: $campos_obligatorios,
