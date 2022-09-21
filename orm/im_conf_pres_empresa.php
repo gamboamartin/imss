69b5cb3f3aa09829bc00598ad17735e548e30d60
MauricioHdz <mauricioisraelhernandez@gmail.com>
2022-09-21 16:19:24 -0500

    v0.35.4 Se integra funcion obten_configuracion_empresa

--- orm/im_conf_pres_empresa.php
+++ orm/im_conf_pres_empresa.php
@@ -1,6 +1,8 @@
 <?php
 namespace models;
 use base\orm\modelo;
+use gamboamartin\empleado\models\em_empleado;
+use gamboamartin\errores\errores;
 use PDO;
 
 class im_conf_pres_empresa extends modelo{
@@ -11,5 +13,20 @@ class im_conf_pres_empresa extends modelo{
 
         parent::__construct(link: $link,tabla:  $tabla, campos_obligatorios: $campos_obligatorios,
             columnas: $columnas);
+    }
+
+    public function obten_configuraciones_empresa(int $em_empleado_id){
+        $org_empresa = (new em_empleado($this->link))->get_empresa(em_empleado_id: $em_empleado_id);
+        if (errores::$error) {
+            return $this->error->error(mensaje: 'Error al obtener registro empresa', data: $org_empresa);
+        }
+
+        $filtro['org_empresa.id'] = $org_empresa['org_empresa_id'];
+        $r_conf_prestaciones = $this->filtro_and(filtro: $filtro);
+        if (errores::$error) {
+            return $this->error->error(mensaje: 'Error al obtener registro configuracion', data: $r_conf_prestaciones);
+        }
+
+        return $r_conf_prestaciones->registros;
     }
 }
