e967c5f196a368a67bc1175c76e76d49341ba165
mgamboa <gamboa.martin@gmail.com>
2022-09-21 09:25:37 -0500

    v.0.29.4 Se integra regimen fiscal

--- orm/im_registro_patronal.php
+++ orm/im_registro_patronal.php
@@ -8,7 +8,8 @@ class im_registro_patronal extends modelo{
         $tabla = __CLASS__;
         $columnas = array($tabla=>false, 'fc_csd' => $tabla, 'org_sucursal' => 'fc_csd',
             'org_empresa' => 'org_sucursal','im_clase_riesgo' => $tabla,'dp_calle_pertenece'=>'org_sucursal',
-            'dp_colonia_postal'=>'dp_calle_pertenece','dp_cp'=>'dp_colonia_postal');
+            'dp_colonia_postal'=>'dp_calle_pertenece','dp_cp'=>'dp_colonia_postal',
+            'cat_sat_regimen_fiscal'=>'org_empresa');
         $campos_obligatorios = array('im_clase_riesgo_id','fc_csd_id','descripcion_select');
 
         parent::__construct(link: $link,tabla:  $tabla, campos_obligatorios: $campos_obligatorios,
