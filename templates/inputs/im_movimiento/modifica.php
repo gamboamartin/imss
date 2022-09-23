dff759712bea3380e4c35c4cb8f01e91cae11497
Kevin <kevin.acuna@ivitec.com.mx>
2022-09-23 11:31:25 -0500

    v.0.43.4 Se ajustan views alta y modifica en entidad movimiento

--- templates/inputs/im_movimiento/modifica.php
+++ templates/inputs/im_movimiento/modifica.php
@@ -1,6 +1,5 @@
 <?php /** @var \gamboamartin\im_registro_patronal\controllers\controlador_im_movimiento  $controlador controlador en ejecucion */ ?>
 <?php use config\views; ?>
-<?php echo $controlador->forms_inputs_modifica; ?>
 <?php echo $controlador->inputs->select->im_tipo_movimiento_id; ?>
 <?php echo $controlador->inputs->select->im_registro_patronal_id; ?>
 <?php echo $controlador->inputs->select->em_empleado_id; ?>
