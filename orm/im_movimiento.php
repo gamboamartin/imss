e519323d3acf382b7d9c0a6a9e6dd261450758bf
MauricioHdz <mauricioisraelhernandez@gmail.com>
2022-09-26 15:43:39 -0500

    v0.49.5 Se integra funcion de calculo prestaciones sociales

--- orm/im_movimiento.php
+++ orm/im_movimiento.php
@@ -286,6 +286,28 @@ class im_movimiento extends modelo{
         return round($total_cuota,2);
     }
 
+    public function calcula_guarderia_prestaciones_sociales(float $factor_pres_sociales, float $n_dias_trabajados,
+                                               float $salario_base_cotizacion): float|array
+    {
+        if($factor_pres_sociales <= 0.0){
+            return $this->error->error("Error el factor debe ser menor a 0", $factor_pres_sociales);
+        }
+        if($salario_base_cotizacion <= 0.0){
+            return $this->error->error("Error salario base de cotizacion debe ser menor a 0",
+                $salario_base_cotizacion);
+        }
+        if($n_dias_trabajados <= 0.0){
+            return $this->error->error("Error los dias trabajados no debe ser menor a 0",
+                $n_dias_trabajados);
+        }
+
+        $cuota_diaria = $factor_pres_sociales * $salario_base_cotizacion;
+        $cuota_diaria = $cuota_diaria/100;
+        $total_cuota = $cuota_diaria * $n_dias_trabajados;
+
+        return round($total_cuota,2);
+    }
+
     public function modifica_bd(array $registro, int $id, bool $reactiva = false): array|stdClass
     {
         $em_empleado = $this->registro_por_id(entidad: new em_empleado($this->link),
