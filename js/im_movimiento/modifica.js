33c9e8f7e19965552660819a037deebf18d65c4f
MauricioHdz <mauricioisraelhernandez@gmail.com>
2022-11-28 17:10:56 -0600

    v0.109.11 Se ajusta js de imss

--- js/im_movimiento/modifica.js
+++ js/im_movimiento/modifica.js
@@ -8,15 +8,11 @@ let txt_salario_diario_integrado = $('#salario_diario_integrado');
 let txt_fecha_inicio_rel_laboral = $('#fecha');
 
 txt_salario_diario.change(function (){
-    let em_empleado_id = sl_em_empleado.val();
-    let fecha_inicio_rel_laboral = txt_fecha_inicio_rel_laboral.val();
     let salario_diario = $(this).val();
-    let url = "index.php?seccion=em_empleado&ws=1&accion=calcula_sdi&em_empleado_id="+em_empleado_id+"&fecha_inicio_rel_laboral="+fecha_inicio_rel_laboral+"&salario_diario="+salario_diario+"&session_id="+session_id;
+    let factor = txt_factor_integracion.val();
 
-    getData(url,(data) => {
-        console.log(data);
-        txt_salario_diario_integrado.val(data);
-    });
+    let res = salario_diario * factor;
+    txt_salario_diario_integrado.val(res);
 });
 
 let getData = async (url, acciones) => {
