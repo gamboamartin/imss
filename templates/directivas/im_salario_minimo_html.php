4b4cbb67a587cf41288bf7965ac558a03146b8aa
[user] <angel.martinez@ivitec.com.mx>
2022-12-29 16:51:15 -0600

    v1.4.0 Se ajusta clase riesgo y salario min

--- templates/directivas/im_salario_minimo_html.php
+++ templates/directivas/im_salario_minimo_html.php
@@ -105,7 +105,7 @@ class im_salario_minimo_html extends html_controler {
         return $inputs;
     }
 
-    public function input_monto(int $cols, stdClass $row_upd, bool $value_vacio, bool $disabled = false):
+    public function input_monto(int $cols, stdClass $row_upd, bool $value_vacio, bool $disabled = false, string $place_holder = 'Monto'):
     array|string
     {
         $valida = $this->directivas->valida_cols(cols: $cols);
