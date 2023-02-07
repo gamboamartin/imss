82bc84e50449d9590a14f77c58032731ab8e9ba3
MauricioHdz <mauricioisraelhernandez@gmail.com>
2023-02-07 13:46:07 -0600

    v1.12.1 Se ajusta alta_bd de mo

--- orm/im_clase_riesgo.php
+++ orm/im_clase_riesgo.php
@@ -1,7 +1,9 @@
 <?php
 namespace gamboamartin\im_registro_patronal\models;
 use base\orm\modelo;
+use gamboamartin\errores\errores;
 use PDO;
+use stdClass;
 
 class im_clase_riesgo extends modelo{
     public function __construct(PDO $link){
@@ -14,5 +16,23 @@ class im_clase_riesgo extends modelo{
 
         $this->NAMESPACE = __NAMESPACE__;
 
+    }
+
+    public function alta_bd(): array|stdClass
+    {
+        if(!isset($this->registro['codigo'])){
+            $this->registro['codigo'] = $this->registro['descripcion'].$this->registro['factor'].' - '.rand();
+        }
+
+        if(!isset($this->registro['codigo_bis'])){
+            $this->registro['codigo_bis'] = $this->registro['codigo'];
+        }
+
+        $alta_bd = parent::alta_bd();
+        if (errores::$error) {
+            return $this->error->error(mensaje: 'Error al insertar clase riesgo', data: $alta_bd);
+        }
+
+        return $alta_bd;
     }
 }
